8f93e9387a5e50612dddf69719184fa2ee79bcac
Stephanie Hester <stephaniehester@Stephanies-MacBook.local>
2017-08-23 22:18:24 -0700

    created game logic

--- Pre-Coursework/MemoryGame/main.js
+++ Pre-Coursework/MemoryGame/main.js
@@ -29,20 +29,33 @@ var checkForMatch = function () {
 	} else {
 		alert("Sorry, try again.");
 	}
-}
+};
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+
 	cardsInPlay.push(cards[cardId].rank);
 	
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-    console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
-}
 
-flipCard([0]);
-flipCard([2]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+};
+
+var createBoard = function () {
+	for (var i=0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
 
